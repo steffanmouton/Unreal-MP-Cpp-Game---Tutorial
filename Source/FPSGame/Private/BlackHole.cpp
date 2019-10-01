e07a7e8c2b893bdbab4751c38a6e439979c67d20
Steffan Mouton <moutonsteffan@gmail.com>
2019-10-01 18:00:50 -0500

    BlackHole Code Written

---
+++ Source/FPSGame/Private/BlackHole.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BlackHole.h"
+
+// Sets default values
+ABlackHole::ABlackHole()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ABlackHole::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABlackHole::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
